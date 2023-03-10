a8b3dd89b6fd7e4f096a2870c23ef0c10c36b215
opassley <opassley@gmail.com>
2023-03-10 05:43:57 -0500

    added external scripts and styles. change_slides()

---
+++ application/views/presenter/assets/presenter_styles.php
@@ -0,0 +1,38 @@
+<style>
+
+.menu {
+  width: 1000%;
+  padding: 20px 0;
+  margin: 0;
+    .menu li {
+      list-style: none;
+      padding: 0 50px; 
+      display: inline-block;
+      transition: 100ms;
+    }
+} 	
+.card .card-header{padding:5px ;    border-radius: 5px;    margin-left: 10px;    margin-right: 15px;    margin-top: -30px;    border: 0;} 
+.card .card-body, .card .card-footer {
+   padding:0;
+}
+#presenter_bible thead td{text-transform:uppercase;padding:0 0 0 12px;font-weight:bold;color:#999;}
+#presenter_bible li{margin:0;}
+#presenter_bible .uk-table tbody td{  padding:12px;} 
+#presenter_bible{ } 
+tbody .uk-list{height:260px; overflow:auto}
+
+.edit-slide-frame{padding-top:0;}
+
+h3#top_bar{
+		background-color:#ffffff5a;
+		font-size:1rem;
+		font-weight:400;
+} 
+
+h3 a{color:#ffffff;padding:0 2px;}
+
+#video_controls{display:none;}
+.hide{display:none;}
+.show{display:inline;}
+.mini_video_controls{cursor:pointer; }
+</style>
--- application/views/presenter/index.php
+++ application/views/presenter/index.php
@@ -375,48 +375,6 @@
 <script>
 	/* ------------- SCRIPTS --------- */
 
-	function change_slide(slide_id,slide_type)
-	{ 
-		var text;
-		if(slide_id == "new-slide-body"){
-			text = document.getElementById(slide_id).value;
-		}
-		else{
-			$("#current_slide").load("https://kpresenter.xyz/presenter2/select_slide/"+slide_id); 
-			//select a slide and update slide number
-			text = $("#slidebody"+slide_id).text(); 
-		//alert(text);  
-		}
-		localStorage.setItem("currentSlide",text);
-		localStorage.setItem("slide_type",slide_type);
-		
-		if(slide_type == "video"){
-			$("#video_controls").removeClass("hide");  
-			$("#video_controls").addClass("show");
-		}
-		else
-			$("#video_controls").addClass("hide");
-			
-		if(localStorage.getItem("slide_type") == "video")
-			localStorage.setItem("slide_type","new_video"); 
-		
-		if(localStorage.getItem("slide_type") == "text")
-			localStorage.setItem("slide_type","text"); 
-		
-		if(localStorage.getItem("slide_type") == "image")
-			localStorage.setItem("slide_type","image"); 
-			
-		$(".slide-tiles li").removeClass("slide-tiles_active");  
-		$("#slide_thumb"+slide_id).addClass("slide-tiles_active");
-		
-		//Assign value to edit current slide container
-		$("#edit-currentslide-body").load("https://kpresenter.xyz/presenter/slide_text/");
-				  
-				  
-				  
-		//$("#edit-currentslide-body").load("https://kpresenter.xyz/presenter/slide_text/");
-	} 
-
 	
  //add song to slideshow
 function add_song(song_id){
